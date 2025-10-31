5c4259135adad82f1ae204167059c90de4e90cb6
Todor Todorov <todorov.t.mail@gmail.com>
2025-10-31 16:51:18 +0200

    CONFIG_AT_ETHERNET_SUPPORT=y

--- components/at/src/at_eth_init.c
+++ components/at/src/at_eth_init.c
@@ -1,13 +1,9 @@
-/*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 #include "esp_netif.h"
 
 #ifdef CONFIG_AT_ETHERNET_SUPPORT
 #include "string.h"
 #include "esp_eth.h"
+#include "driver/gpio.h"
 
 #define CONFIG_AT_ETH_MDC_GPIO         23
 #define CONFIG_AT_ETH_MDIO_GPIO        18
@@ -22,8 +18,8 @@ bool esp_at_get_eth_default_config(esp_eth_config_t* config)
     phy_config.phy_addr = CONFIG_AT_ETH_PHY_ADDR;
     phy_config.reset_gpio_num = CONFIG_AT_ETH_PHY_RST_GPIO;
     eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
-    esp32_emac_config.smi_gpio.mdc_num = CONFIG_AT_ETH_MDC_GPIO;
-    esp32_emac_config.smi_gpio.mdio_num = CONFIG_AT_ETH_MDIO_GPIO;
+    esp32_emac_config.smi_mdc_gpio_num = CONFIG_AT_ETH_MDC_GPIO;
+    esp32_emac_config.smi_mdio_gpio_num = CONFIG_AT_ETH_MDIO_GPIO;
     esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
 
 #if CONFIG_PHY_IP101
@@ -32,6 +28,15 @@ bool esp_at_get_eth_default_config(esp_eth_config_t* config)
     esp_eth_phy_t *phy = esp_eth_phy_new_rtl8201(&phy_config);
 #elif CONFIG_PHY_LAN8720
     esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
+    
+	gpio_config_t io_enable_osc={
+		.intr_type = GPIO_INTR_DISABLE,
+		.mode = GPIO_MODE_OUTPUT,
+		.pin_bit_mask = (1 << 16)
+	};
+	gpio_config(&io_enable_osc);
+    gpio_set_level(16, 1);
+
 #elif CONFIG_PHY_DP83848
     esp_eth_phy_t *phy = esp_eth_phy_new_dp83848(&phy_config);
 #endif
